08501ad9dc4d00474bd2591502266b387e48b8a4
Simon O'Keefe <simon.okeefe@groundtruth.com.au>
2014-03-05 23:11:52 +1100

    Add description for duplication function

--- tools/Duplicate GIS-on-a-Stick.bat
+++ tools/Duplicate GIS-on-a-Stick.bat
@@ -1,4 +1,6 @@
+@echo off
+echo This operation will copy the GIS-on-a-Stick files and folders
+echo to the drive you specify.
 set /P destinationdrive="Enter destination drive letter: "
-
 xcopy ..\*.* %destinationdrive%:\ /D /E /EXCLUDE:exclusions.txt
 pause
